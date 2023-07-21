6e56fb045f029b0d844ea7b48d6907eea97f21bb
MilanMarcetic <marcetic18@hotmail.com>
2023-07-21 21:40:00 +0200

    Fly io deployment

--- API/Program.cs
+++ API/Program.cs
@@ -19,13 +19,28 @@ builder.Services.AddCors();
 builder.Services.AddIdentityServices(builder.Configuration);
 
 var connString = "";
-if (builder.Environment.IsDevelopment())
+if (builder.Environment.IsDevelopment()) // local
 {
     connString = builder.Configuration.GetConnectionString("DefaultConnection");
 }
-else // prod
+else // production
 {
-    connString = Environment.GetEnvironmentVariable("CONN_URL");
+    // Use connection string provided at runtime by Fly.io.
+    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+    // Parse connection URL to connection string for Npgsql
+    connUrl = connUrl.Replace("postgres://", string.Empty);
+    var pgUserPass = connUrl.Split("@")[0];
+    var pgHostPortDb = connUrl.Split("@")[1];
+    var pgHostPort = pgHostPortDb.Split("/")[0];
+    var pgDb = pgHostPortDb.Split("/")[1];
+    var pgUser = pgUserPass.Split(":")[0];
+    var pgPass = pgUserPass.Split(":")[1];
+    var pgHost = pgHostPort.Split(":")[0];
+    var pgPort = pgHostPort.Split(":")[1];
+    var updatedHost = pgHost.Replace("flycast", "internal");
+
+    connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
 }
 builder.Services.AddDbContext<DataContext>(opt =>
 {
